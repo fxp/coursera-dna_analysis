0517cb13d847925bc802f1595611204a53e6ed74
Xiaoping Feng <fxp007@gmail.com>
2016-08-12 10:26:41 +0800

    Update 1.3 Some Hidden Messages are More Surprising than Others.js

--- 1.3 Some Hidden Messages are More Surprising than Others.js
+++ 1.3 Some Hidden Messages are More Surprising than Others.js
@@ -7,3 +7,19 @@ for(var i =0;i<strand.length;i++){
   result+=output[i]
 }
 console.log(result)
+
+
+
+function findPattern(text, pattern){
+  var founds= [];
+  for(var i =0;i<text.length - pattern.length+1;i++){
+    if(text.substring(i,i+ pattern.length) == pattern){
+      founds.push(i);  
+    }
+  }
+  var result="";
+  for(var i =0;i<founds.length;i++){
+    result+=founds[i]+" ";
+  }
+  return result
+}
