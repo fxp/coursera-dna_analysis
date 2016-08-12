fcae69996df499000b8cb18c35699d5c14dfa2f7
Xiaoping Feng <fxp007@gmail.com>
2016-08-12 10:52:39 +0800

    Update 1.3 Some Hidden Messages are More Surprising than Others.js

--- 1.3 Some Hidden Messages are More Surprising than Others.js
+++ 1.3 Some Hidden Messages are More Surprising than Others.js
@@ -23,3 +23,42 @@ function findPattern(text, pattern){
   }
   return result
 }
+
+function fetchAndAnalyze(pattern){
+  fetch('https://stepic.org/media/attachments/lessons/3/Vibrio_cholerae.txt', {
+  	method: 'get'
+  }).then(function(response) {
+  	return response.text();
+  }).then(function(text) {
+    findPattern(text, pattern);
+  }).catch(function(err) {
+  	// Error :(
+  });
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+
+function findPattern(text, pattern){
+  var founds= [];
+  for(var i =0;i<text.length - pattern.length+1;i++){
+    if(text.substring(i,i+ pattern.length) == pattern){
+      founds.push(i);
+    }
+  }
+  var result="";
+  for(var i =0;i<founds.length;i++){
+    result+=founds[i]+" ";
+  }
+  return result
+}
+
+var total = "";
+process.stdin.on('data', function(chunk) {
+  total +=chunk;
+});
+process.stdin.on('end', function() {
+  var result = findPattern(total, "CTTGATCAT");
+  console.log(result);
+});
